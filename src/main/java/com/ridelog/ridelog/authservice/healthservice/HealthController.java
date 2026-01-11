93bfa785b3ba5eec7624fe625de8a30842e9d7da
tanmaysahu09 <tanmaysahu09@gmail.com>
2026-01-11 22:03:15 +0530

    "Remove application.properties from latest commit" and added HealthController

--- src/main/java/com/ridelog/ridelog/authservice/healthservice/HealthController.java
+++ src/main/java/com/ridelog/ridelog/authservice/healthservice/HealthController.java
@@ -1,4 +1,15 @@
 package com.ridelog.ridelog.authservice.healthservice;
 
-public class hea {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "OK";
+    }
 }
